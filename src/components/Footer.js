9e94a7328e0b82a92e43c89a75e62b5829c29d6d
Donymmello <sidonioaly@gmail.com>
2024-11-21 13:29:14 +0200

    fixing footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,36 +1,39 @@
 import React from 'react';
-import { Box, IconButton, Typography } from '@mui/material';
+import { Box, IconButton, Typography, useTheme } from '@mui/material';
 import { FaFacebook, FaInstagram, FaLinkedin } from 'react-icons/fa';
 
 function Footer() {
+  const theme = useTheme();  // Acessa o tema atual
+
   return (
-    <Box 
+    <Box
       sx={{
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
         flexDirection: 'column',
         padding: '1rem',
-        backgroundColor: '#f8f8f8',
+        backgroundColor: theme.palette.background.paper,  // Usa a cor de fundo do tema
+        color: theme.palette.text.primary,  // Usa a cor de texto do tema
       }}
     >
-      <Typography variant="body1" color="textSecondary" gutterBottom>
-      &copy; 2024 African Lifting Machinery Entity. All rights reserved.
+      <Typography variant="body1" gutterBottom>
+        &copy; 2024 African Lifting Machinery Entity. All rights reserved.
       </Typography>
-      <Typography variant="body1" color="textSecondary" gutterBottom>
+      <Typography variant="body1" gutterBottom>
         Connect with us
       </Typography>
       <Box sx={{ textAlign: 'center', mt: 4 }}>
-      <IconButton color="primary" href="https://facebook.com">
-        <FaFacebook />
-      </IconButton>
-      <IconButton color="primary" href="https://instagram.com">
-        <FaInstagram />
-      </IconButton>
-      <IconButton color="primary" href="https://linkedin.com">
-        <FaLinkedin />
-      </IconButton>
-    </Box>
+        <IconButton color="primary" href="https://facebook.com">
+          <FaFacebook />
+        </IconButton>
+        <IconButton color="primary" href="https://instagram.com">
+          <FaInstagram />
+        </IconButton>
+        <IconButton color="primary" href="https://linkedin.com">
+          <FaLinkedin />
+        </IconButton>
+      </Box>
     </Box>
   );
 }
